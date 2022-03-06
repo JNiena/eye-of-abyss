28f45e60031c77d2314f7fadd7d39d34eb78e041
JNiena <JNiena@protonmail.com>
2022-03-06 16:15:38 -0500

    Example

--- source/bot.ts
+++ source/bot.ts
@@ -1,3 +1,5 @@
 import {Whitelist} from "./whitelist";
+import {MinecraftBot} from "./minecraftbot";
 
 let whitelist : Whitelist = new Whitelist();
+let mcbot : MinecraftBot = new MinecraftBot("email@email.com", "mypassword", "play.someserver.com");
