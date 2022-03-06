ef09e30d3f54fa1332bfeac8e1f01d0dee6016a5
JNiena <JNiena@protonmail.com>
2022-03-06 18:12:13 -0500

    Bot loader to load discord config and minecraft account config

---
+++ source/botloader.ts
@@ -0,0 +1,19 @@
+import {Files} from "./files";
+import {Config} from "./config";
+
+export class Botloader {
+
+	static loadFromFile<T>(path: string): T {
+		return new Object(new Config(path)) as T;
+	}
+
+	static loadFromDirectory<T>(path: string): T[] {
+		let configPaths: string[] = Files.readAll(path);
+		let accounts: T[] = [];
+		for (let i = 0; i < configPaths.length; i++) {
+			accounts.push(this.loadFromFile<T>(configPaths[i]));
+		}
+		return accounts;
+	}
+
+}
