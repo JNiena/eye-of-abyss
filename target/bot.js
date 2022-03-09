72970c0d3e2c6fe9b548f50458bc64b2ba1487ae
JNiena <JNiena@protonmail.com>
2022-03-08 20:12:24 -0500

    latest

--- target/bot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const whitelist_1 = require("./whitelist");
-const minecraftbot_1 = require("./minecraftbot");
-let whitelist = new whitelist_1.Whitelist();
-let mcbot = new minecraftbot_1.MinecraftBot("nienabjs@gmail.com", "N13n4b3r", "play.hylioncraft.com");
-mcbot.onMessage((message) => {
-    console.log(message);
-});
