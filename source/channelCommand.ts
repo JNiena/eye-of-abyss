9e8ac145af0d686a862d38792c95c6d8176d6e7e
JNiena <JNiena@protonmail.com>
2022-03-11 12:51:04 -0500

    Convenience class for channel commands

---
+++ source/channelCommand.ts
@@ -0,0 +1,18 @@
+import {Command} from "./command";
+
+export class ChannelCommand extends Command {
+
+	private channelID: string;
+
+	constructor(channelID: string, name: string, handler: Function) {
+		super(name, handler);
+		this.channelID = channelID;
+	}
+
+	handle(message: any) {
+		if (message.channel.id === this.channelID) {
+			super.handle(message);
+		}
+	}
+
+}
