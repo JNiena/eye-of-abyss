f2bae0ffd096f9ee5950ed3713c99ba5d6b7f0c4
JNiena <JNiena@protonmail.com>
2022-03-12 16:13:59 -0500

    Bot will attempt to auto reconnect on an error

--- source/listeners/ErrorListener.ts
+++ source/listeners/ErrorListener.ts
@@ -6,9 +6,15 @@ export class ErrorListener implements EventListener {
 
 	public handle: any;
 
-	constructor(channelID: string, discordBot: DiscordBot, minecraftBot: MinecraftBot) {
+	constructor(channelID: string, discordBot: DiscordBot, minecraftBot: MinecraftBot, setupBehavior: Function) {
 		this.handle = (error: Error) => {
 			discordBot.send(`<@&${minecraftBot.config.get()["discord"]["pingRoleID"]}> **The bot has encountered an error! Reason: ${error.message}**`, channelID).then();
+			if (minecraftBot.config.get()["autoRejoin"]["enabled"]) {
+				discordBot.send(`**Attempting to reconnect in ${minecraftBot.config.get()["autoRejoin"]["delay"] / 1000} seconds...**`, channelID).then();
+				minecraftBot.reconnect(() => {
+					setupBehavior(minecraftBot);
+				});
+			}
 		};
 	}
 
