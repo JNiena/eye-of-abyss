8cebb865ab053a2e2b3f746c6ed7b879609ea2fa
JNiena <JNiena@protonmail.com>
2022-03-15 17:28:20 -0400

    Merged channel command to command

--- source/Command.ts
+++ source/Command.ts
@@ -1,15 +1,19 @@
 export class Command {
 
+	public channelID: string;
 	public name: string;
 	private handler: Function;
 
-	constructor(name: string, handler: Function) {
+	constructor(channelID: string, name: string, handler: Function) {
+		this.channelID = channelID;
 		this.name = name;
 		this.handler = handler;
 	}
 
 	handle(message: any): void {
+		message.content = message.content.replace(this.name, "");
 		this.handler(message);
+		message.content = `${this.name}${message.content}`;
 	}
 
 }
