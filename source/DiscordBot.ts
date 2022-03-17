0576804612f22b5fced84b71d8bc3d12cdf7e287
JNiena <JNiena@protonmail.com>
2022-03-16 23:18:20 -0400

    Connection callback

--- source/DiscordBot.ts
+++ source/DiscordBot.ts
@@ -1,21 +1,34 @@
 import {Intents, TextChannel} from "discord.js";
-import {AkairoClient, CommandHandler} from "discord-akairo";
+import {AkairoClient, Command, CommandHandler} from "discord-akairo";
 import {Config} from "./Config";
 
 export class DiscordBot extends AkairoClient {
 
 	private commandHandler: CommandHandler;
 
-	constructor(config: Config) {
+	public constructor(config: Config) {
 		super({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
 		this.commandHandler = new CommandHandler(this, {
 			"directory": config.get()["commands"]["path"],
 			"prefix": config.get()["commands"]["prefix"]
 		});
 		this.commandHandler.loadAll();
+		this.token = config.get()["token"];
 	}
 
-	async send(text: string, channelID: string): Promise<any> {
+	public connect(callback: Function = () => {}): void {
+		if (this.token) {
+			this.login(this.token).then(() => {
+				callback();
+			});
+		}
+	}
+
+	public registerCommand(command: Command): void {
+		this.commandHandler.register(command);
+	}
+
+	public async send(text: string, channelID: string): Promise<any> {
 		if (text.length === 0) return Promise.resolve();
 		try {
 			let channel = this.channels.cache.get(channelID);
