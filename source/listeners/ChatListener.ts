67f40e1c38a0b508b504102156f1245485029ea4
JNiena <JNiena@protonmail.com>
2022-03-20 18:12:01 -0400

    Reformat

--- source/listeners/ChatListener.ts
+++ source/listeners/ChatListener.ts
@@ -11,12 +11,8 @@ export class ChatListener implements EventListener {
 	public constructor(channelID: string, discordBot: DiscordBot, minecraftBot: MinecraftBot) {
 		this.handle = (username: string, message: string) => {
 			let toSend: string = username + " » " + message.replace("@", "");
-			if (minecraftBot.config.get()["log"]["enabled"]) {
-				Files.write(minecraftBot.config.get()["log"]["path"], toSend + "\n");
-			}
-			if (!minecraftBot.config.get()["whitelist"]["enabled"] || new Whitelist(minecraftBot.config.get()["whitelist"]["filter"]).processText(toSend)) {
-				discordBot.send(toSend, channelID).then();
-			}
+			if (minecraftBot.config.get()["log"]["enabled"]) Files.write(minecraftBot.config.get()["log"]["path"], toSend + "\n");
+			if (!minecraftBot.config.get()["whitelist"]["enabled"] || new Whitelist(minecraftBot.config.get()["whitelist"]["filter"]).processText(toSend)) discordBot.send(toSend, channelID).then();
 		};
 	}
 
