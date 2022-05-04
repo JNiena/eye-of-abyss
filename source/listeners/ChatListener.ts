395cc78492d65ece8c834777db575dfbcbf3c040
JNiena <JNiena@protonmail.com>
2022-05-04 15:36:57 -0400

    Disabled discord mentions. Added ping option to chat trigger

--- source/listeners/ChatListener.ts
+++ source/listeners/ChatListener.ts
@@ -9,18 +9,20 @@ export class ChatListener {
 	public constructor(minecraftBot: MinecraftBot, discordBot: DiscordBot) {
 		let config: Config = minecraftBot.config;
 		let channelID: string = config.get()["discord"]["channelID"];
+		let roleID: string = config.get()["discord"]["roleID"];
 
 		minecraftBot.on("chat", (username: string, message: string) => {
 			let filter: Filter | undefined;
 			if (config.get()["filter"]["enable"]) filter = new Filter(config.get()["filter"]["list"]);
 
-			let formattedMessage: string = `${username} » ${message}`;
+			let formattedMessage: string = `${username} » ${message.replace(/@/g, "")}`;
 			if (config.get()["logging"]["enable"]) Files.write(config.get()["logging"]["path"], `${formattedMessage}\n`);
 			if (filter === undefined || filter.complies(`${username} ${message}`)) discordBot.send(formattedMessage, channelID).then();
 
 			for (let i = 0; i < config.get()["chat"].length; i++) {
 				if (message.toLowerCase().includes(config.get()["chat"][i]["trigger"].toLowerCase())) {
 					minecraftBot.chat(config.get()["chat"][i]["reply"], config.get()["chat"][i]["delay"]);
+					if (config.get()["chat"][i]["ping"]) discordBot.send(`<@&${roleID}>`, channelID).then();
 				}
 			}
 		});
