88f333b12c6262a56b405a65e3aeb5f9480accd6
Jack Nienaber <JNiena@protonmail.com>
2022-12-03 02:39:45 -0500

    Bot now reconnects on error

--- source/listeners/ErrorListener.ts
+++ source/listeners/ErrorListener.ts
@@ -1,16 +1,18 @@
-import {MinecraftBot} from "../MinecraftBot";
-import {DiscordBot} from "../DiscordBot";
-import {Config} from "../Config";
+import { MinecraftBot } from "../MinecraftBot";
+import { DiscordBot } from "../DiscordBot";
+import { Config } from "../Config";
 
 export class ErrorListener {
 
 	public constructor(minecraftBot: MinecraftBot, discordBot: DiscordBot) {
 		let config: Config = minecraftBot.config;
 		let channelID: string = config.get()["discord"]["channelID"];
-		let roleID: string = config.get()["discord"]["roleID"];
-
 		minecraftBot.on("error", (error: Error) => {
-			discordBot.send(`<@&${roleID}> **ERROR: ${error.message}**`, channelID).then();
+			discordBot.send(`**(BOT) ERROR: ${error.message}**`, channelID).then();
+			if (config.get()["rejoin"]["enable"]) {
+				discordBot.send(`**(BOT) RECONNECTING: ${config.get()["rejoin"]["delay"] / 1000}s**`, channelID).then();
+				minecraftBot.connect(config.get()["rejoin"]["delay"]);
+			}
 		});
 	}
 
