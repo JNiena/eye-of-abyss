76b320ecabd769a62744107e686f685797c9dda7
Jack Nienaber <JNiena@protonmail.com>
2022-12-03 02:39:18 -0500

    Added "(BOT)" header to responses. Reformat

--- source/listeners/DeathListener.ts
+++ source/listeners/DeathListener.ts
@@ -1,17 +1,16 @@
-import {MinecraftBot} from "../MinecraftBot";
-import {DiscordBot} from "../DiscordBot";
-import {Config} from "../Config";
+import { MinecraftBot } from "../MinecraftBot";
+import { DiscordBot } from "../DiscordBot";
+import { Config } from "../Config";
 
 export class DeathListener {
 
 	public constructor(minecraftBot: MinecraftBot, discordBot: DiscordBot) {
 		let config: Config = minecraftBot.config;
 		let channelID: string = config.get()["discord"]["channelID"];
-
 		minecraftBot.on("death", () => {
-			discordBot.send("**KILLED**", channelID).then();
+			discordBot.send("**(BOT) KILLED**", channelID).then();
 			if (config.get()["events"]["death"]["enable"]) {
-				discordBot.send(`**RESPAWNING: ${config.get()["events"]["death"]["delay"] / 1000}s**`, channelID).then();
+				discordBot.send(`**(BOT) RESPAWNING: ${config.get()["events"]["death"]["delay"] / 1000}s**`, channelID).then();
 				minecraftBot.chat(config.get()["events"]["death"]["message"], config.get()["events"]["death"]["delay"]);
 			}
 		});
