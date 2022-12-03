2505deb85c003e8bd6941fe7045c05b2e935b614
Jack Nienaber <JNiena@protonmail.com>
2022-12-03 02:40:42 -0500

    Added "(BOT)" header to responses. Reformat

--- source/listeners/KickedListener.ts
+++ source/listeners/KickedListener.ts
@@ -1,18 +1,16 @@
-import {MinecraftBot} from "../MinecraftBot";
-import {DiscordBot} from "../DiscordBot";
-import {Config} from "../Config";
+import { MinecraftBot } from "../MinecraftBot";
+import { DiscordBot } from "../DiscordBot";
+import { Config } from "../Config";
 
 export class KickedListener {
 
 	public constructor(minecraftBot: MinecraftBot, discordBot: DiscordBot) {
 		let config: Config = minecraftBot.config;
 		let channelID: string = config.get()["discord"]["channelID"];
-		let roleID: string = config.get()["discord"]["roleID"];
-
 		minecraftBot.on("kicked", (reason: string) => {
-			discordBot.send(`<@&${roleID}> **DISCONNECTED: ${reason}**`, channelID).then();
+			discordBot.send(`**(BOT) DISCONNECTED: ${reason}**`, channelID).then();
 			if (config.get()["rejoin"]["enable"]) {
-				discordBot.send(`**RECONNECTING: ${config.get()["rejoin"]["delay"] / 1000}s**`, channelID).then();
+				discordBot.send(`**(BOT) RECONNECTING: ${config.get()["rejoin"]["delay"] / 1000}s**`, channelID).then();
 				minecraftBot.connect(config.get()["rejoin"]["delay"]);
 			}
 		});
--- source/listeners/SpawnedListener.ts
+++ source/listeners/SpawnedListener.ts
@@ -1,15 +1,14 @@
-import {MinecraftBot} from "../MinecraftBot";
-import {DiscordBot} from "../DiscordBot";
-import {Config} from "../Config";
+import { MinecraftBot } from "../MinecraftBot";
+import { DiscordBot } from "../DiscordBot";
+import { Config } from "../Config";
 
 export class SpawnedListener {
 
 	public constructor(minecraftBot: MinecraftBot, discordBot: DiscordBot) {
 		let config: Config = minecraftBot.config;
 		let channelID: string = config.get()["discord"]["channelID"];
-
 		minecraftBot.once("spawn", () => {
-			discordBot.send("**CONNECTED**", channelID).then();
+			discordBot.send("**(BOT) CONNECTED**", channelID).then();
 			if (config.get()["events"]["join"]["enable"]) {
 				minecraftBot.chat(config.get()["events"]["join"]["message"], config.get()["events"]["join"]["delay"]);
 			}
