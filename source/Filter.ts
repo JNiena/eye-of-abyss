8fd38d8a07d4a649b41d482feb9329e96b72dc3e
Jack Nienaber <JNiena@protonmail.com>
2022-12-18 22:50:15 -0500

    Added filter phrase supports

--- source/Filter.ts
+++ source/Filter.ts
@@ -24,11 +24,25 @@ export class Filter {
 
 	public complies(element: string): boolean {
 		for (let i = 0; i < this.words.length; i++) {
-			if (element.toLowerCase().includes(this.words[i].toLowerCase())) {
-				return true;
-			}
+			return this.words[i].includes("|")
+				? this.handle_phrase(element.toLowerCase(), this.words[i].toLowerCase())
+				: this.handle_word(element.toLowerCase(), this.words[i].toLowerCase());
 		}
 		return false;
+	}
+
+	private handle_phrase(test: string, word: string): boolean {
+		let words: string[] = word.split("|");
+		for (let i = 0; i < words.length; i++) {
+			if (!test.includes(words[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private handle_word(test: string, word: string): boolean {
+		return test.includes(word);
 	}
 
 }
