899192ec74daff0da1d214cdc76117cd3e5f090e
Jack Nienaber <JNiena@protonmail.com>
2023-05-09 18:11:00 -0400

    Removed redundant embed

--- source/minecraft/JoinListener.ts
+++ source/minecraft/JoinListener.ts
@@ -1,10 +1,8 @@
-import { Embeds } from "../Embeds";
-import { config, discordBot, minecraftBot } from "../Main";
+import { config, minecraftBot } from "../Main";
 
 export class JoinListener {
 	public constructor() {
 		minecraftBot.once("spawn", () => {
-			discordBot.sendEmbed(Embeds.connected()).then();
 			if (config.get().events.join.enable) {
 				minecraftBot.chat(config.get().events.join.message, config.get().events.join.delay);
 			}
