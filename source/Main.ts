597120de80b7906f61283f1be5c12299a04755fb
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 21:50:19 -0400

    Reorganized structure

--- source/Main.ts
+++ source/Main.ts
@@ -1,53 +1,40 @@
-import { DiscordBot } from "./DiscordBot";
+import { LogLevel } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
 import { Config } from "./Config";
+import { DiscordClient } from "./DiscordClient";
+import { ChatListener } from "./minecraft/ChatListener";
+import { DeathListener } from "./minecraft/DeathListener";
+import { ErrorListener } from "./minecraft/ErrorListener";
+import { JoinListener } from "./minecraft/JoinListener";
+import { KickedListener } from "./minecraft/KickedListener";
 import { MinecraftBot } from "./MinecraftBot";
-import { ErrorListener } from "./listeners/ErrorListener";
-import { ChatListener } from "./listeners/ChatListener";
-import { SpawnedListener } from "./listeners/SpawnedListener";
-import { KickedListener } from "./listeners/KickedListener";
-import { DeathListener } from "./listeners/DeathListener";
-import { ConnectCommand } from "./commands/ConnectCommand";
-import { DisconnectCommand } from "./commands/DisconnectCommand";
-import { ReconnectCommand } from "./commands/ReconnectCommand";
-import { SayCommand } from "./commands/SayCommand";
-import { StatusCommand } from "./commands/StatusCommand";
-import { FilterAddCommand } from "./commands/filter/FilterAddCommand";
-import { FilterRemoveCommand } from "./commands/filter/FilterRemoveCommand";
-import { FilterEnableCommand } from "./commands/filter/FilterEnableCommand";
-import { FilterDisableCommand } from "./commands/filter/FilterDisableCommand";
-import { FilterResetCommand } from "./commands/filter/FilterResetCommand";
-import { FilterListCommand } from "./commands/filter/FilterListCommand";
-import { ChannelInhibitor } from "./inhibitors/ChannelInhibitor";
-import { PluginManager } from "./PluginManager";
 import { Repeater } from "./Repeater";
 
-let pluginManager: PluginManager = new PluginManager();
-let config: Config = new Config(process.argv[2]);
+const config: Config = new Config("config.json");
 
-let discordBot: DiscordBot = new DiscordBot(config);
-let minecraftBot: MinecraftBot = new MinecraftBot(config, () => {
-	new ErrorListener(minecraftBot, discordBot);
-	new ChatListener(minecraftBot, discordBot);
-	new SpawnedListener(minecraftBot, discordBot);
-	new KickedListener(minecraftBot, discordBot);
-	new DeathListener(minecraftBot, discordBot);
-	pluginManager.loadDirectory("./plugins", minecraftBot, discordBot);
+const discordBot: DiscordClient = new DiscordClient({
+	"intents": [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+	"loadMessageCommandListeners": true,
+	"loadDefaultErrorListeners": true,
+	"loadSubcommandErrorListeners": true,
+	"loadPlugins": false,
+	"logger": { "level": LogLevel.Debug }
 });
-new Repeater(minecraftBot);
+
+const minecraftBot: MinecraftBot = new MinecraftBot(() => {
+	new ErrorListener();
+	new ChatListener();
+	new JoinListener();
+	new KickedListener();
+	new DeathListener();
+});
+Repeater.start(minecraftBot);
 
 minecraftBot.connect();
+discordBot.login(config.get().discord.token).then();
 
-discordBot.registerCommand(new ConnectCommand(minecraftBot));
-discordBot.registerCommand(new DisconnectCommand(minecraftBot));
-discordBot.registerCommand(new ReconnectCommand(minecraftBot));
-discordBot.registerCommand(new StatusCommand(minecraftBot));
-discordBot.registerCommand(new SayCommand(minecraftBot));
-discordBot.registerCommand(new FilterListCommand(minecraftBot));
-discordBot.registerCommand(new FilterAddCommand(minecraftBot));
-discordBot.registerCommand(new FilterRemoveCommand(minecraftBot));
-discordBot.registerCommand(new FilterResetCommand(minecraftBot));
-discordBot.registerCommand(new FilterEnableCommand(minecraftBot));
-discordBot.registerCommand(new FilterDisableCommand(minecraftBot));
-discordBot.registerInhibitor(new ChannelInhibitor(config));
-
-discordBot.start();
+export {
+	config,
+	discordBot,
+	minecraftBot
+};
