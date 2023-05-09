1c88c055ecc8e8009fed138993b6b56a6c4261d8
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 21:21:25 -0400

    Renamed methods

--- source/Filter.ts
+++ source/Filter.ts
@@ -1,40 +1,17 @@
 export class Filter {
-
-	private words: string[];
-
-	public constructor(words: string[] = []) {
-		this.words = words;
-	}
-
-	public add(word: string): void {
-		this.words.push(word.toLowerCase());
-	}
-
-	public remove(word: string): boolean {
-		if (!this.words.includes(word.toLowerCase())) {
-			return false;
-		}
-		this.words = this.words.filter(element => element !== word.toLowerCase());
-		return true;
-	}
-
-	public get(): string[] {
-		return this.words;
-	}
-
-	public complies(element: string): boolean {
-		for (let i = 0; i < this.words.length; i++) {
-			if (this.words[i].includes("|") && this.handle_phrase(element.toLowerCase(), this.words[i].toLowerCase())) {
+	public static complies(words: string[], element: string): boolean {
+		for (let i = 0; i < words.length; i++) {
+			if (words[i].includes("|") && this.testWithPhrase(element.toLowerCase(), words[i].toLowerCase())) {
 				return true;
 			}
-			else if (this.handle_word(element.toLowerCase(), this.words[i].toLowerCase())) {
+			else if (this.testWithWord(element.toLowerCase(), words[i].toLowerCase())) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private handle_phrase(test: string, word: string): boolean {
+	private static testWithPhrase(test: string, word: string): boolean {
 		let words: string[] = word.split("|");
 		for (let i = 0; i < words.length; i++) {
 			if (!test.includes(words[i])) {
@@ -44,8 +21,7 @@ export class Filter {
 		return true;
 	}
 
-	private handle_word(test: string, word: string): boolean {
+	private static testWithWord(test: string, word: string): boolean {
 		return test.includes(word);
 	}
-
 }
