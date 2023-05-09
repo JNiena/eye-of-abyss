527c9b8ffdba8cf5ec9cf79e7e791b1df44085a9
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 21:06:55 -0400

    Added error messages

---
+++ source/listeners/chatInputCommandDenied.ts
@@ -0,0 +1,15 @@
+import { type ChatInputCommandDeniedPayload, Events, Listener, type UserError } from "@sapphire/framework";
+
+export class ChatInputCommandDenied extends Listener<typeof Events.ChatInputCommandDenied> {
+	public run(error: UserError, { interaction }: ChatInputCommandDeniedPayload) {
+		if (interaction.deferred || interaction.replied) {
+			return interaction.editReply({
+				"content": error.message
+			});
+		}
+		return interaction.reply({
+			"content": error.message,
+			"ephemeral": true
+		});
+	}
+}
