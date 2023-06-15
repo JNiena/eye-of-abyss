711ba91437de80cb49d4f18c08911bbd0e45a563
Jack Nienaber <JNiena@protonmail.com>
2023-06-15 17:48:59 -0400

    Removed useless check

--- source/Repeater.ts
+++ source/Repeater.ts
@@ -4,7 +4,7 @@ export class Repeater {
 	public static start(): void {
 		for (let i: number = 0; i < config.get().repeater.length; i++) {
 			setInterval(() => {
-				if (minecraftBot && minecraftBot.isConnected()) {
+				if (minecraftBot.isConnected()) {
 					minecraftBot.chat(config.get().repeater[i].message);
 				}
 			}, config.get().repeater[i].interval);
