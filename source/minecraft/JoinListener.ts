0bd22bf3c1c620fdd329731990d7273bf12c3a68
Jack Nienaber <JNiena@protonmail.com>
2023-06-15 18:01:12 -0400

    Spawn listener works between connects

--- source/minecraft/JoinListener.ts
+++ source/minecraft/JoinListener.ts
@@ -2,7 +2,7 @@ import { config, minecraftBot } from "../Main";
 
 export class JoinListener {
 	public constructor() {
-		minecraftBot.once("spawn", () => {
+		minecraftBot.on("spawn", () => {
 			if (config.get().events.join.enable) {
 				minecraftBot.chat(config.get().events.join.message, config.get().events.join.delay);
 			}
