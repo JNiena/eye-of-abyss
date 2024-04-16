e6e2edcd23fb1804ef8faa91098b6fbfc441163c
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 16:57:50 -0400

    Added handler for chatInputRun commands

--- src/preconditions/PluginEnabled.ts
+++ src/preconditions/PluginEnabled.ts
@@ -1,13 +1,14 @@
 import { Precondition } from "@sapphire/framework";
-import { Message } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 import { config } from "../Main";
 
 export class PluginEnabled extends Precondition {
-	public async messageRun(message: Message<boolean>) {
-		const name: string = message.content.includes(" ")
-			? message.content.substring(1, message.content.indexOf(" "))
-			: message.content.substring(1);
-		return config.get().plugins.includes(name)
+	public async chatInputRun(interaction: ChatInputCommandInteraction) {
+		return this.isEnabled(interaction.commandName);
+	}
+
+	private async isEnabled(commandName: string) {
+		return config.get().plugins.includes(commandName)
 			? this.ok()
 			: this.error();
 	}
@@ -18,5 +19,3 @@ declare module "@sapphire/framework" {
 		PluginEnabled: never;
 	}
 }
-
-export default undefined;
