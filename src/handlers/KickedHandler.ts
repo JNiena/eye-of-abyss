9aa2f73c2de37806e77f122276b66287da39ac54
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:01:00 -0400

    Doesn't attempt to reconnect when already connected on another client

--- src/handlers/KickedHandler.ts
+++ src/handlers/KickedHandler.ts
@@ -2,6 +2,9 @@ import { minecraftBot } from "../Main";
 
 export class KickedHandler {
 	public constructor() {
-		minecraftBot.internal.on("kicked", (reason: string) => { minecraftBot.lastLog = { "type": "Kicked", "message": reason }; });
+		minecraftBot.internal.on("kicked", (reason: string) => {
+			minecraftBot.lastLog = { "type": "Kicked", "message": reason };
+			if (reason.includes("already connected to this proxy")) { clearTimeout(minecraftBot.autoReconnect); }
+		});
 	}
 }
