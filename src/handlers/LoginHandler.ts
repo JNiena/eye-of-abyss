053d3a26647d978c4785afeebbbb4bba62fe55cb
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:00:07 -0400

    Auto set username if it can

--- src/handlers/LoginHandler.ts
+++ src/handlers/LoginHandler.ts
@@ -1,13 +1,12 @@
 import { Embeds } from "../Embeds";
 import { config, discordBot, minecraftBot } from "../Main";
-import { MinecraftBot } from "../MinecraftBot";
 
 export class LoginHandler {
 	public constructor() {
 		minecraftBot.internal.on("login", () => {
 			minecraftBot.connected = true;
-			if (!MinecraftBot.username) { MinecraftBot.username = minecraftBot.internal.username; }
-			discordBot.sendEmbed(Embeds.connected(MinecraftBot.username), config.get().discord.infoChannelID).then();
+			if (minecraftBot.internal.username && !config.get().credentials.username) { config.get().credentials.username = minecraftBot.internal.username; }
+			discordBot.sendEmbed(Embeds.connected(), config.get().discord.infoChannelID).then();
 		});
 	}
 }
