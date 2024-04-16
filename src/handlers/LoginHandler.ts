6b63cec4f3a77413396650ccef445e65f1fcb380
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 18:49:59 -0400

    Fixed duplicate connection message when bot connects to the proxy server + real server

--- src/handlers/LoginHandler.ts
+++ src/handlers/LoginHandler.ts
@@ -2,14 +2,21 @@ import { Embeds } from "../Embeds";
 import { config, discordBot, minecraftBot } from "../Main";
 
 export class LoginHandler {
+	private canSend: boolean;
+
 	public constructor() {
+		this.canSend = true;
 		minecraftBot.internal.on("login", () => {
 			minecraftBot.connected = true;
 			if (minecraftBot.internal.username && !config.get().credentials.username) {
 				config.get().credentials.username = minecraftBot.internal.username;
 				config.save();
 			}
-			discordBot.sendEmbed(Embeds.connected(), config.get().discord.infoChannelID);
+			if (this.canSend) {
+				this.canSend = false;
+				discordBot.sendEmbed(Embeds.connected(), config.get().discord.infoChannelID);
+				setTimeout(() => { this.canSend = true; }, 30_000);
+			}
 		});
 	}
 }
