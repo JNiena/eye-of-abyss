edcd86202b8252b6741022b6929b0db03c3a8c5d
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:26:26 -0400

    Reformat

--- src/handlers/ErrorHandler.ts
+++ src/handlers/ErrorHandler.ts
@@ -4,8 +4,7 @@ export class ErrorHandler {
 	public constructor() {
 		minecraftBot.internal.on("error", (error: Error) => {
 			minecraftBot.lastLog = { "type": "Error", "message": error.message };
-			if (error.message.includes("client timed out")) { return; }
-			clearTimeout(minecraftBot.autoReconnect);
+			if (!error.message.includes("client timed out")) { minecraftBot.allowAutoReconnect = false; }
 		});
 	}
 }
