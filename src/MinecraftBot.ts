810ab975f4ea246a94e47a79e141fbefafdaafe5
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:02:04 -0400

    Startup now takes an array of functions

--- src/MinecraftBot.ts
+++ src/MinecraftBot.ts
@@ -6,10 +6,9 @@ export class MinecraftBot {
 	public autoReconnect: NodeJS.Timeout | undefined;
 	public lastLog: Log | undefined;
 	public internal!: Bot;
-	public startup: Function;
-	public static username: string | undefined;
+	public startup: Function[];
 
-	public constructor(startup: Function) {
+	public constructor(startup: Function[]) {
 		this.connected = false;
 		this.lastLog = undefined;
 		this.startup = startup;
@@ -18,16 +17,11 @@ export class MinecraftBot {
 	public connect() {
 		this.internal = createBot({ "username": config.get().credentials.email, "password": config.get().credentials.password, "auth": config.get().credentials.auth, "host": config.get().server.host, "port": config.get().server.port, "version": config.get().server.version });
 		if (!this.autoReconnect) { this.autoReconnect = setInterval(() => { if (config.get().autoreconnect.enable) { if (!this.connected) { this.connect(); } } }, config.get().autoreconnect.interval); }
-		this.startup();
+		for (const startup of this.startup) { startup(); }
 	}
 
 	public disconnect() {
 		this.internal.quit();
-	}
-
-	public reconnect() {
-		if (this.connected) { this.disconnect(); }
-		this.connect();
 	}
 
 	public chat(message: string) {
