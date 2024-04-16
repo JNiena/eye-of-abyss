8907702232361c1c0a2c96773fea446df28075d6
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:59:17 -0400

    Now saves the username to config

--- src/handlers/LoginHandler.ts
+++ src/handlers/LoginHandler.ts
@@ -5,7 +5,10 @@ export class LoginHandler {
 	public constructor() {
 		minecraftBot.internal.on("login", () => {
 			minecraftBot.connected = true;
-			if (minecraftBot.internal.username && !config.get().credentials.username) { config.get().credentials.username = minecraftBot.internal.username; }
+			if (minecraftBot.internal.username && !config.get().credentials.username) {
+				config.get().credentials.username = minecraftBot.internal.username;
+				config.save();
+			}
 			discordBot.sendEmbed(Embeds.connected(), config.get().discord.infoChannelID).then();
 		});
 	}
