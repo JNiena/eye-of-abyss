39891d64088e05f2118dbd1da23664f3ba0d7f43
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:26:15 -0400

    Added a second check

--- src/handlers/KickedHandler.ts
+++ src/handlers/KickedHandler.ts
@@ -4,8 +4,7 @@ export class KickedHandler {
 	public constructor() {
 		minecraftBot.internal.on("kicked", (reason: string) => {
 			minecraftBot.lastLog = { "type": "Kicked", "message": reason };
-			if (!reason.includes("already connected to this proxy")) { return; }
-			clearTimeout(minecraftBot.autoReconnect);
+			if (reason.includes("already connected to this proxy") || reason.includes("duplicate_login")) { minecraftBot.allowAutoReconnect = false; }
 		});
 	}
 }
