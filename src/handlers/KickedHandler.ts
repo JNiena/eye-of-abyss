68af3643f148a8674efbd11179a4d1eec68df253
Jack Nienaber <JNiena@protonmail.com>
2024-04-16 17:07:48 -0400

    Reformat

--- src/handlers/KickedHandler.ts
+++ src/handlers/KickedHandler.ts
@@ -4,7 +4,8 @@ export class KickedHandler {
 	public constructor() {
 		minecraftBot.internal.on("kicked", (reason: string) => {
 			minecraftBot.lastLog = { "type": "Kicked", "message": reason };
-			if (reason.includes("already connected to this proxy")) { clearTimeout(minecraftBot.autoReconnect); }
+			if (!reason.includes("already connected to this proxy")) { return; }
+			clearTimeout(minecraftBot.autoReconnect);
 		});
 	}
 }
